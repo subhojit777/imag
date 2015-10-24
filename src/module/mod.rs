911384331bdaddca774945c0272a4c1c2f969f16
Matthias Beyer <mail@beyermatthias.de>
2015-10-24 18:40:40 +0200

    Add ModuleError

--- src/module/mod.rs
+++ src/module/mod.rs
@@ -1,19 +1,57 @@
 use runtime::Runtime;
 use std::error::Error;
+use std::fmt::Formatter;
+use std::fmt::Result as FMTResult;
+use std::fmt::Display;
 use std::path::Path;
+use std::result::Result;
 
 use module::todo::TodoModule;
 
 mod todo;
 
+#[derive(Debug)]
+pub struct ModuleError {
+    desc: String,
+}
+
+impl ModuleError {
+    fn mk(desc: String) -> ModuleError {
+        ModuleError {
+            desc: desc,
+        }
+    }
+}
+
+impl Error for ModuleError {
+
+    fn description(&self) -> &str {
+        &self.desc[..]
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        None
+    }
+
+}
+
+impl Display for ModuleError {
+    fn fmt(&self, f: &mut Formatter) -> FMTResult {
+        write!(f, "ModuleError: {}",
+               self.description())
+    }
+}
+
+pub type ModuleResult = Result<(), ModuleError>;
+
 pub trait Module {
 
-    fn new(&rt : Runtime) -> Self;
-    fn callnames() -> &'static [str];
+    fn new(rt : &Runtime) -> Self;
+    fn callnames() -> &'static [&'static str];
     fn name(&self) -> &'static str;
 
-    fn execute(&self, &rt : Runtime) -> Option<Error>;
-    fn shutdown(&self, &rt : Runtime) -> Option<Error>;
+    fn execute(&self, rt : &Runtime) -> ModuleResult;
+    fn shutdown(&self, rt : &Runtime) -> ModuleResult;
 
 }
 
