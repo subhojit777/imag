c41ec1d85c82fab967cd4ec64ce13b6fc55377de
Matthias Beyer <mail@beyermatthias.de>
2015-10-26 20:53:12 +0100

    Add logger with logging library

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #[macro_use] extern crate clap;
+#[macro_use] extern crate log;
 
 use cli::Config;
-use runtime::Runtime;
+use runtime::{ImagLogger, Runtime};
 use clap::App;
 
 mod cli;
@@ -10,10 +11,12 @@ mod module;
 mod storage;
 
 fn main() {
+    let early_logger = ImagLogger::early().unwrap();
     let yaml = load_yaml!("../etc/cli.yml");
     let app = App::from_yaml(yaml);
     let mut config = Config::new(app);
 
+    let logger = ImagLogger::init(&config);
     let rt = Runtime::new(config);
 
     println!("Hello, world!");
