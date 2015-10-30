e426f384f45f6d86b9e19b5fff87cfd7731e9ac5
Matthias Beyer <mail@beyermatthias.de>
2015-10-30 14:47:21 +0100

    Remove old code

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -7,13 +7,3 @@ pub use runtime::Runtime;
 mod file;
 mod parser;
 
-pub trait StorageBackend {
-
-    fn name(&self) -> String;
-
-    fn create(&self, file : File)   -> Option<Error>;
-    fn read(&self, path: Path)      -> Result<File, Error>;
-    fn update(&self, file : File)   -> Option<Error>;
-    fn destroy(&self, path: Path)   -> Option<Error>;
-
-}
