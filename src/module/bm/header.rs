8cffdddab2506cc539b4e1496d586ac1e5b9f270
Matthias Beyer <mail@beyermatthias.de>
2015-10-31 12:13:21 +0100

    Split header spec into smaller functions

--- src/module/bm/header.rs
+++ src/module/bm/header.rs
@@ -1,15 +1,18 @@
 use storage::file::FileHeaderSpec as FHS;
 
 pub fn get_spec() -> FHS {
-    FHS::Map {
-        keys: vec![
-            FHS::Key { name: "URL", value_type: Box::new(FHS::Text) },
-            FHS::Key { name: "TAGS",
-                  value_type: Box::new(FHS::Array {
-                      allowed_types: vec![FHS::Text]
-                  }),
-            },
-        ],
-    }
+    FHS::Map { keys: vec![ url_key(), tags_key() ] }
+}
+
+fn url_key() -> FHS {
+    FHS::Key { name: "URL", value_type: Box::new(FHS::Text) }
+}
+
+fn tags_key() -> FHS {
+    FHS::Key { name: "TAGS", value_type: Box::new(text_array()) }
+}
+
+fn text_array() -> FHS {
+    FHS::Array { allowed_types: vec![FHS::Text] }
 }
 
