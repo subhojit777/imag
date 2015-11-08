cd72295c1a5e56a2f5c2b4612a5b8cb310413bcc
Matthias Beyer <mail@beyermatthias.de>
2015-11-08 17:11:48 +0100

    Add function to build header content

--- src/module/bm/header.rs
+++ src/module/bm/header.rs
@@ -1,4 +1,5 @@
 use storage::file::FileHeaderSpec as FHS;
+use storage::file::FileHeaderData as FHD;
 
 pub fn get_spec() -> FHS {
     FHS::Map { keys: vec![ url_key(), tags_key() ] }
@@ -14,5 +15,21 @@ fn tags_key() -> FHS {
 
 fn text_array() -> FHS {
     FHS::Array { allowed_types: vec![FHS::Text] }
+}
+
+
+pub fn build_header(url: &String, tags: &Vec<String>) -> FHD {
+    FHD::Map {
+        keys: vec![
+            FHD::Key {
+                name: "URL",
+                value: Box::new(FHD::Text(url.clone()))
+            },
+            FHD::Key {
+                name: "TAGS",
+                value: Box::new(FHD::Text(tags.connect(",")))
+            }
+        ]
+    }
 }
 
