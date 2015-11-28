e10d1bcb659f43afa07766c1127abbf318a1a5ed
Matthias Beyer <mail@beyermatthias.de>
2015-11-28 11:09:50 +0100

    Reimplement commands for bm

--- src/module/bm/commands/mod.rs
+++ src/module/bm/commands/mod.rs
@@ -1,92 +1,14 @@
-mod add {
-    use clap::ArgMatches;
+use runtime::Runtime;
 
-    use module::command::{CommandError, CommandResult, CommandEnv, ExecutableCommand};
-    use runtime::Runtime;
-    use storage::backend::{StorageBackend, StorageBackendError};
-
-    pub struct AddCommand;
-
-    pub impl AddCommand {
-
-        fn new() -> AddCommand {
-            AddCommand
-        }
-
-    }
-
-    pub impl ExecutableCommand for AddCommand {
-
-        fn get_callname() -> &'static str {
-            "add"
-        }
-
-        fn exec<'a>(&self, env: CommandEnv<'a>) -> CommandResult
-        {
-        }
-
-    }
-
+pub fn add_command(rt: &Runtime) {
+    unimplemented!()
 }
 
-mod list {
-    use clap::ArgMatches;
-
-    use module::command::{CommandError, CommandResult, CommandEnv, ExecutableCommand};
-    use runtime::Runtime;
-    use storage::backend::{StorageBackend, StorageBackendError};
-
-    pub struct ListCommand;
-
-    pub impl ListCommand {
-
-        fn new() -> ListCommand {
-            ListCommand
-        }
-
-    }
-
-    pub impl ExecutableCommand for ListCommand {
-
-        fn get_callname() -> &'static str {
-            "list"
-        }
-
-        fn exec<'a>(&self, env: CommandEnv<'a>) -> CommandResult
-        {
-        }
-
-    }
-
+pub fn list_command(rt: &Runtime) {
+    unimplemented!()
 }
 
-mod remove {
-    use clap::ArgMatches;
-
-    use module::command::{CommandError, CommandResult, CommandEnv, ExecutableCommand};
-    use runtime::Runtime;
-    use storage::backend::{StorageBackend, StorageBackendError};
-
-    pub struct RemoveCommand;
-
-    pub impl RemoveCommand {
-
-        fn new() -> RemoveCommand {
-            RemoveCommand
-        }
-
-    }
-
-    pub impl ExecutableCommand for RemoveCommand {
-
-        fn get_callname() -> &'static str {
-            "remove"
-        }
-
-        fn exec<'a>(&self, env: CommandEnv<'a>) -> CommandResult
-        {
-        }
-
-    }
-
+pub fn remove_command(rt: &Runtime) {
+    unimplemented!()
 }
+
