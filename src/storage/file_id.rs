2c3dd521b8815ab3ef54aa17b72fa2dcc8de436a
Matthias Beyer <mail@beyermatthias.de>
2015-12-02 13:49:19 +0100

    Add test: file_id_from_string()

--- src/storage/file_id.rs
+++ src/storage/file_id.rs
@@ -148,3 +148,52 @@ impl<'a> Display for FileIDError {
 
 pub type FileIDResult = Result<FileID, FileIDError>;
 
+#[cfg(test)]
+mod test {
+
+    use super::{FileID, FileIDType};
+
+    #[test]
+    fn file_id_from_string() {
+        setup_logger();
+
+        let s1 = String::from("/home/user/testmodule-UUID-some-id.imag");
+        let s2 = String::from("/home/user/testmodule-UUID-some-id.extension.imag");
+        let s3 = String::from("/home/user/testmodule-NOHASH-some-id.imag");
+
+        let id1 = FileID::from(s1);
+        let id2 = FileID::from(s2);
+        let id3 = FileID::from(s3);
+
+        println!("Id 1 : {:?}", id1);
+        println!("Id 2 : {:?}", id2);
+        println!("Id 3 : {:?}", id3);
+
+        assert_eq!(FileIDType::UUID, id1.get_type());
+        assert_eq!(FileIDType::UUID, id2.get_type());
+        assert_eq!(FileIDType::NONE, id3.get_type());
+
+        let f1 : String = id1.into();
+        let f2 : String = id2.into();
+        let f3 : String = id3.into();
+
+        assert_eq!(String::from("some-id"), f1);
+        assert_eq!(String::from("some-id"), f2);
+        assert_eq!(String::from("INVALID"), f3);
+    }
+
+    fn setup_logger() {
+        extern crate log;
+        use log::{LogLevelFilter, set_logger};
+        use runtime::ImagLogger;
+
+        log::set_logger(|max_log_lvl| {
+            let lvl = LogLevelFilter::Debug;
+            max_log_lvl.set(lvl);
+            Box::new(ImagLogger::new(lvl.to_log_level().unwrap()))
+        });
+        debug!("Init logger for test");
+    }
+
+}
+
