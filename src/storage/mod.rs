cb2c512440f81fc1339acf836581a96400d6539b
Matthias Beyer <mail@beyermatthias.de>
2015-12-27 23:03:57 +0100

    Add Store type

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,4 +1,26 @@
+use std::cell::RefCell;
+use std::collections::HashMap;
+
 pub mod path;
 pub mod file;
 pub mod parser;
 pub mod json;
+
+use storage::file::File;
+use storage::file::id::FileID;
+
+type Cache<'a> = HashMap<FileID, RefCell<File<'a>>>;
+
+pub struct Store<'a> {
+    cache : RefCell<Cache<'a>>,
+}
+
+impl<'a> Store<'a> {
+
+    pub fn new() -> Store<'a> {
+        Store {
+            cache: RefCell::new(HashMap::new()),
+        }
+    }
+
+}
