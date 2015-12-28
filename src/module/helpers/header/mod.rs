c6f31496ab476dce38203c08b376a1b3f9dcfb8f
Matthias Beyer <mail@beyermatthias.de>
2015-12-28 12:15:05 +0100

    Module helpers: header: Add get_url_from_header()

--- src/module/helpers/header/mod.rs
+++ src/module/helpers/header/mod.rs
@@ -4,3 +4,35 @@
 
 pub mod tags;
 
+pub mod data {
+    use std::ops::Deref;
+    use storage::file::header::data::FileHeaderData as FHD;
+
+    pub fn get_url_from_header(header: &FHD) -> Option<String> {
+        match header {
+            &FHD::Map{keys: ref ks} => {
+                let mut keys : Vec<FHD> = ks.clone();
+                keys.iter().find(|k| {
+                    match k.deref() {
+                        &FHD::Key{name: ref n, value: ref v} => n == "URL",
+                        _ => false
+                    }
+                }).and_then(|urlkey| {
+                    match urlkey.deref().clone() {
+                        FHD::Text(s) => Some(s),
+                        _ => {
+                            warn!("Malformed Header Data: Expected Text, found non-Text");
+                            None
+                        },
+                    }
+                })
+            },
+            _ => {
+                warn!("Malformed Header Data: Expected Map, found non-Map");
+                None
+            }
+        }
+    }
+
+}
+
--- src/module/helpers/header/tags.rs
+++ src/module/helpers/header/tags.rs
@@ -63,6 +63,5 @@ pub mod data {
         tags
     }
 
-
 }
 
