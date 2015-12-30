eed57af0bb498c618069767e4e69fa5e36d0da75
Matthias Beyer <mail@beyermatthias.de>
2015-12-30 11:32:42 +0100

    module header helpers: Add function to get NAME from header

--- src/module/helpers/header/mod.rs
+++ src/module/helpers/header/mod.rs
@@ -52,5 +52,46 @@ pub mod data {
         }
     }
 
+    /**
+     * Get an NAME from a header, whereas the header has to have the following format:
+     *
+     *   { ..., "NAME": "<NAME>", ... }
+     *
+     * Does no spec verification.
+     */
+    pub fn get_name_from_header(header: &FHD) -> Option<String> {
+        match header {
+            &FHD::Map{keys: ref ks} => {
+                let mut keys : Vec<FHD> = ks.clone();
+                keys.iter().find(|k| {
+                    match k.deref() {
+                        &FHD::Key{name: ref n, value: ref v} => n == "NAME",
+                        _ => false
+                    }
+                }).and_then(|urlkey| {
+                    match urlkey.deref().clone() {
+                        FHD::Key{name: _, value: ref v} => {
+                            match v.deref().clone() {
+                                FHD::Text(s) => Some(s),
+                                _ => {
+                                    warn!("Malformed Header Data: Expected Text, found non-Text");
+                                    None
+                                },
+                            }
+                        }
+                        _ => {
+                            warn!("Malformed Header Data: Expected Text, found non-Text");
+                            None
+                        },
+                    }
+                })
+            },
+            _ => {
+                warn!("Malformed Header Data: Expected Map, found non-Map");
+                None
+            }
+        }
+    }
+
 }
 
