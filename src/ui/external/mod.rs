62ced34964e50cbb6ec473b7180cfba1d37d14f4
Matthias Beyer <mail@beyermatthias.de>
2016-01-03 17:19:29 +0100

    Add helper: get_tempfile()

--- src/ui/external/mod.rs
+++ src/ui/external/mod.rs
@@ -1,1 +1,14 @@
+use std::fs::File;
+
 pub mod editor;
+
+pub fn get_tempfile(ext: &str) -> Option<(String, File)> {
+    use rand::random;
+
+    let randomname = format!("/tmp/imag-{}.{}", random::<u64>(), ext);
+    debug!("Attempting to create tempfile at {}", randomname);
+    File::create(randomname.clone())
+        .map_err(|e| debug!(" Error -> {}", e))
+        .ok()
+        .map(|f| (randomname, f))
+}
