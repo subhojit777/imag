e233073305f85516412111f0b458541240c820c5
Matthias Beyer <mail@beyermatthias.de>
2016-01-05 16:17:05 +0100

    Remove unneeded Keyword "mut"

--- src/storage/file/id.rs
+++ src/storage/file/id.rs
@@ -68,9 +68,9 @@ impl FileID {
             }
             debug!("Matches: {}", capts.len());
 
-            let modname     = capts.at(1).unwrap();
-            let hashname    = capts.at(2).unwrap();
-            let mut hash    = capts.at(3).unwrap();
+            let modname  = capts.at(1).unwrap();
+            let hashname = capts.at(2).unwrap();
+            let hash     = capts.at(3).unwrap();
 
             debug!("Destructure FilePath to ID:");
             debug!("                  FilePath: {:?}", s);
