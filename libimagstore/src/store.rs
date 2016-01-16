a7d1cdadc03f6176df945d540aac9a49fdffc4e8
Julian Ganz <neither@nut.email>
2016-01-16 07:15:49 +0100

    Add initial version of store interface
    The interface enables users to both create and remove entries. It also
    features methods for retrieval of both unlocked and lockes entries and a
    method for writing back the latter one only.

---
+++ libimagstore/src/store.rs
@@ -0,0 +1,17 @@
+use std::result::Result as RResult;
+use std::string;
+
+pub use entry::Entry;
+pub use error::StoreError;
+
+pub type Result<T> = RResult<T, StoreError>;
+pub type LockedEntry = SingleUseLock<Entry>;
+
+pub trait Store {
+    fn create(&self, entry : Entry) -> Result<()>;
+    fn retrieve(&self, id : string) -> Result<LockedEntry>;
+    fn retrieve_copy(&self, id : string) -> Result<Entry>;
+    fn update(&self, LockedEntry) -> Result<()>;
+    fn delete(&self, id : string) -> Result<()>;
+}
+
