ba0ef701e91f663ed3f4b1d67e4cb16e5e40e897
Julian Ganz <neither@nut.email>
2016-01-16 15:11:20 +0100

    Use strings correctly

--- libimagstore/src/store.rs
+++ libimagstore/src/store.rs
@@ -1,5 +1,5 @@
 use std::result::Result as RResult;
-use std::string;
+use std::string::String;
 
 pub use entry::Entry;
 pub use error::StoreError;
@@ -10,9 +10,9 @@ pub type LockedEntry = SingleUseLock<Entry>;
 
 pub trait Store {
     fn create(&self, entry : Entry) -> Result<()>;
-    fn retrieve(&self, id : string) -> Result<LockedEntry>;
-    fn retrieve_copy(&self, id : string) -> Result<Entry>;
+    fn retrieve(&self, id : String) -> Result<LockedEntry>;
+    fn retrieve_copy(&self, id : String) -> Result<Entry>;
     fn update(&self, LockedEntry) -> Result<()>;
-    fn delete(&self, id : string) -> Result<()>;
+    fn delete(&self, id : String) -> Result<()>;
 }
 
