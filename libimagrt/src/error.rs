6b2502c4d705bc6c43de0a02ca1af69811f92999
Matthias Beyer <mail@beyermatthias.de>
2016-01-21 18:42:43 +0100

    rt: Add error for instantiation

---
+++ libimagrt/src/error.rs
@@ -0,0 +1,57 @@
+use std::error::Error;
+use std::fmt::Display;
+use std::fmt::Formatter;
+use std::fmt::Error as FmtError;
+use std::clone::Clone;
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum RuntimeErrorKind {
+    Instantiate,
+
+    // more?
+}
+
+#[derive(Debug)]
+pub struct RuntimeError {
+    kind: RuntimeErrorKind,
+    cause: Option<Box<Error>>,
+}
+
+impl RuntimeError {
+
+    pub fn new(kind: RuntimeErrorKind, cause: Option<Box<Error>>) -> RuntimeError {
+        RuntimeError {
+            kind: kind,
+            cause: cause,
+        }
+    }
+
+}
+
+fn runtime_error_kind_as_str(e: &RuntimeErrorKind) -> &'static str {
+    match e {
+        &RuntimeErrorKind::Instantiate => "Could not instantiate",
+    }
+}
+
+impl Display for RuntimeError {
+
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), FmtError> {
+        try!(write!(fmt, "{}", runtime_error_kind_as_str(&self.kind)));
+        Ok(())
+    }
+
+}
+
+impl Error for RuntimeError {
+
+    fn description(&self) -> &str {
+        runtime_error_kind_as_str(&self.kind)
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        self.cause.as_ref().map(|e| &**e)
+    }
+
+}
+
