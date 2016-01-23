86e810cc43d307ebcd6b75da03545de4ed02d0a4
Matthias Beyer <mail@beyermatthias.de>
2016-01-23 15:38:14 +0100

    Add dependencies

--- imag-store/src/main.rs
+++ imag-store/src/main.rs
@@ -1,3 +1,12 @@
+extern crate clap;
+#[macro_use] extern crate log;
+extern crate toml;
+#[macro_use] extern crate version;
+
+extern crate libimagrt;
+extern crate libimagstore;
+extern crate libimagutil;
+
 fn main() {
     println!("Hello, world!");
 }
