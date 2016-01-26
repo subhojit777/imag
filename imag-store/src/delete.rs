947d7900bf48a724aa7717e067ea4e81d4aa82ce
Matthias Beyer <mail@beyermatthias.de>
2016-01-26 19:59:55 +0100

    Implement Store::delete() interface

--- imag-store/src/delete.rs
+++ imag-store/src/delete.rs
@@ -1,5 +1,33 @@
+use std::path::PathBuf;
+
 use libimagrt::runtime::Runtime;
 
+use util::build_entry_path;
+
 pub fn delete(rt: &Runtime) {
+    use std::process::exit;
+
+    rt.cli()
+        .subcommand_matches("delete")
+        .map(|sub| {
+            sub.value_of("id")
+                .map(|id| {
+                    debug!("Deleting file at {:?}", id);
+                    rt.store()
+                        .delete(build_entry_path(rt, id))
+                        .map_err(|e| {
+                           warn!("Error: {:?}", e);
+                           exit(1);
+                        })
+                })
+                .or_else(|| {
+                    warn!("No ID passed. Will exit now");
+                    exit(1);
+                })
+        })
+        .or_else(|| {
+            warn!("No subcommand 'delete'. Will exit now");
+            exit(1);
+        });
 }
 
