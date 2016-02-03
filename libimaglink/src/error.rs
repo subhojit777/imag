e6e9a1a19e856a0981c7f99ef8a4b5a9d34cf519
Matthias Beyer <mail@beyermatthias.de>
2016-02-03 19:59:01 +0100

    Add Error kinds

--- libimaglink/src/error.rs
+++ libimaglink/src/error.rs
@@ -6,11 +6,21 @@ use std::fmt;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum LinkErrorKind {
+    EntryHeaderReadError,
+    EntryHeaderWriteError,
+    ExistingLinkTypeWrong,
 }
 
 fn link_error_type_as_str(e: &LinkErrorKind) -> &'static str {
     match e {
-        _ => unimplemented!(),
+        &LinkErrorKind::EntryHeaderReadError
+            => "Error while reading an entry header",
+
+        &LinkErrorKind::EntryHeaderWriteError
+            => "Error while writing an entry header",
+
+        &LinkErrorKind::ExistingLinkTypeWrong
+            => "Existing link entry has wrong type",
     }
 }
 
