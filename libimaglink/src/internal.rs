a53d36c23df9987e3456cb7c4b814adedeb0a87e
Matthias Beyer <mail@beyermatthias.de>
2016-02-07 16:40:33 +0100

    Implement internal::set_links()

--- libimaglink/src/internal.rs
+++ libimaglink/src/internal.rs
@@ -10,8 +10,11 @@ pub fn get_links(header: &EntryHeader) -> Result<Links> {
     process_rw_result(header.read("imag.links"))
 }
 
+/// Set the links in a header and return the old links, if any.
 pub fn set_links(header: &mut EntryHeader, links: Links) -> Result<Links> {
-    unimplemented!()
+    let links : Vec<Link> = links.into();
+    let links : Vec<Value> = links.into_iter().map(|link| Value::String(link.into())).collect();
+    process_rw_result(header.set("imag.links", Value::Array(links)))
 }
 
 pub fn add_link(header: &mut EntryHeader, link: Link) -> Result<()> {
