31b966b9b435cf1e56b30330dc7e091933852fad
Matthias Beyer <mail@beyermatthias.de>
2016-02-12 19:28:20 +0100

    Add check::has_tag{,s}()

---
+++ libimagtag/src/check.rs
@@ -0,0 +1,14 @@
+
+use libimagstore::store::Entry;
+
+use result::Result;
+use tag::Tag;
+
+pub fn has_tag(e: &Entry, t: &Tag) -> Result<bool> {
+    unimplemented!()
+}
+
+pub fn has_tags(e: &Entry, ts: &Vec<Tag>) -> Result<bool> {
+    unimplemented!()
+}
+
