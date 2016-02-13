d21e11e3a53811b537fd3bf4c231ecffe8aea100
Matthias Beyer <mail@beyermatthias.de>
2016-02-13 18:17:07 +0100

    Add dependencies

--- libimagnotes/src/lib.rs
+++ libimagnotes/src/lib.rs
@@ -1,3 +1,8 @@
-#[test]
-fn it_works() {
-}
+#[macro_use] extern crate log;
+extern crate semver;
+extern crate toml;
+
+extern crate libimagrt;
+extern crate libimagstore;
+extern crate libimagtag;
+
