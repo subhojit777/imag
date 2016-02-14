1ca9597ad62a92beaca11d4b945787ff02bf6dd6
Matthias Beyer <mail@beyermatthias.de>
2016-02-14 20:15:03 +0100

    Implement: add::add_tag()

--- libimagtag/src/add.rs
+++ libimagtag/src/add.rs
@@ -1,8 +1,43 @@
+use toml::Value;
+
 use libimagstore::store::Entry;
 
 use result::Result;
 use tag::Tag;
-pub fn add_tag(e: &mut Entry, t: &Tag) -> Result<()> {
+use error::{TagError, TagErrorKind};
 
-    unimplemented!()
+pub fn add_tag(e: &mut Entry, t: &Tag) -> Result<()> {
+    let tags = e.get_header().read("imag.tags");
+    if tags.is_err() {
+        let kind = TagErrorKind::HeaderReadError;
+        return Err(TagError::new(kind, Some(Box::new(tags.err().unwrap()))));
+    }
+    let tags = tags.unwrap();
+
+    if !tags.iter().all(|t| match t { &Value::String(_) => true, _ => false }) {
+        return Err(TagError::new(TagErrorKind::TagTypeError, None));
+    }
+
+    if tags.is_none() {
+        return Ok(());
+    }
+    let tags = tags.unwrap();
+
+    if !match tags { Value::Array(_) => true, _ => false } {
+        return Err(TagError::new(TagErrorKind::TagTypeError, None));
+    }
+
+    match tags {
+        Value::Array(tag_array) => {
+            let mut new_tags = tag_array.clone();
+            new_tags.push(Value::String(t.clone()));
+
+            e.get_header_mut()
+                .set("imag.tags", Value::Array(new_tags))
+                .map_err(|e| TagError::new(TagErrorKind::TagTypeError, Some(Box::new(e))))
+                .map(|_| ())
+        },
+
+        _ => unreachable!(),
+    }
 }
