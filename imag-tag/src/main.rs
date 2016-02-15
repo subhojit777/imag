5898b64e80c6e05eea0c9904ccccf9721509089f
Matthias Beyer <mail@beyermatthias.de>
2016-02-15 14:33:30 +0100

    imag-tag: init

---
+++ imag-tag/src/main.rs
@@ -0,0 +1,14 @@
+extern crate clap;
+#[macro_use] extern crate log;
+extern crate semver;
+extern crate toml;
+#[macro_use] extern crate version;
+
+extern crate libimagstore;
+extern crate libimagrt;
+extern crate libimagtag;
+extern crate libimagutil;
+
+fn main() {
+    println!("Hello, world!");
+}
