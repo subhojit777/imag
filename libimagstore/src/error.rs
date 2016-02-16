3cf25881042cfd2f81dd689be9de8847acfac45c
Matthias Beyer <mail@beyermatthias.de>
2016-02-16 20:42:36 +0100

    Add error types for pre/post hooks

--- libimagstore/src/error.rs
+++ libimagstore/src/error.rs
@@ -28,6 +28,8 @@ pub enum StoreErrorKind {
     HeaderKeyNotFound,
     HeaderTypeFailure,
     HookRegisterError,
+    PreHookExecuteError,
+    PostHookExecuteError,
         // maybe more
 }
 
@@ -52,6 +54,8 @@ fn store_error_type_as_str(e: &StoreErrorKind) -> &'static str {
         &StoreErrorKind::HeaderKeyNotFound     => "Header Key not found",
         &StoreErrorKind::HeaderTypeFailure     => "Header type is wrong",
         &StoreErrorKind::HookRegisterError     => "Hook register error",
+        &StoreErrorKind::PreHookExecuteError   => "Pre-Hook execution error",
+        &StoreErrorKind::PostHookExecuteError  => "Post-Hook execution error",
     }
 }
 
