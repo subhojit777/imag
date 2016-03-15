d75595e4d8d366f7c75535351d24bdd947261421
Matthias Beyer <mail@beyermatthias.de>
2016-03-15 23:11:30 +0100

    Add VersionEq filter

---
+++ libimagentryfilter/src/builtin/header/version/eq.rs
@@ -0,0 +1,44 @@
+use semver::Version;
+use toml::Value;
+
+use libimagstore::store::Entry;
+
+use builtin::header::field_path::FieldPath;
+use filter::Filter;
+
+pub struct VersionEq {
+    version: Version,
+}
+
+impl VersionEq {
+
+    pub fn new(version: Version) -> VersionEq {
+        VersionEq { version: version }
+    }
+
+}
+
+impl Filter for VersionEq {
+
+    fn filter(&self, e: &Entry) -> bool {
+        e.get_header()
+            .read("imag.version")
+            .map(|val| {
+                val.map(|v| {
+                    match v {
+                        Value::String(s) => {
+                            match Version::parse(&s[..]) {
+                                Ok(v) => v == self.version,
+                                _ => false
+                            }
+                        },
+                        _ => false,
+                    }
+                })
+                .unwrap_or(false)
+            })
+            .unwrap_or(false)
+    }
+
+}
+
