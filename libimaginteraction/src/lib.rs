b9769f4fe6e7fa4bade3cb16c74e987a2671b880
Matthias Beyer <mail@beyermatthias.de>
2016-03-19 20:44:20 +0100

    Initial import

---
+++ libimaginteraction/src/lib.rs
@@ -0,0 +1,14 @@
+extern crate spinner;
+extern crate interactor;
+#[macro_use] extern crate log;
+extern crate ansi_term;
+#[macro_use] extern crate lazy_static;
+extern crate regex;
+
+extern crate libimagentryfilter;
+extern crate libimagstore;
+#[macro_use] extern crate libimagutil;
+
+pub mod ask;
+pub mod filter;
+
