c53cc56d0cf6aab33b92b675af726302a300db19
Matthias Beyer <mail@beyermatthias.de>
2016-03-25 16:23:51 +0100

    Enable warning-to-error deny!()

--- libimagrt/src/lib.rs
+++ libimagrt/src/lib.rs
@@ -1,3 +1,17 @@
+#![deny(
+    non_camel_case_types,
+    non_snake_case,
+    path_statements,
+    trivial_numeric_casts,
+    unstable_features,
+    unused_allocation,
+    unused_import_braces,
+    unused_imports,
+    unused_mut,
+    unused_qualifications,
+    while_true,
+)]
+
 #[macro_use] extern crate log;
 #[macro_use] extern crate itertools;
 #[cfg(unix)] extern crate xdg_basedir;
