87fa9bab13da5ab07b9996ad82374d0496c74087
Matthias Beyer <mail@beyermatthias.de>
2016-03-25 16:19:57 +0100

    Make result used

--- libimagrt/src/logger.rs
+++ libimagrt/src/logger.rs
@@ -26,7 +26,7 @@ impl Log for ImagLogger {
     fn log(&self, record: &LogRecord) {
         if self.enabled(record.metadata()) {
             // TODO: This is just simple logging. Maybe we can enhance this lateron
-            writeln!(stderr(), "[imag][{: <5}]: {}", record.level(), record.args());
+            writeln!(stderr(), "[imag][{: <5}]: {}", record.level(), record.args()).ok();
         }
     }
 }
