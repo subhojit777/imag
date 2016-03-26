6f31e0f821415e9dac0d19e6c0a10e7b76ceb57e
Matthias Beyer <mail@beyermatthias.de>
2016-03-26 18:25:17 +0100

    libimagdiary: init

---
+++ libimagdiary/src/lib.rs
@@ -0,0 +1,10 @@
+#[macro_use] extern crate log;
+extern crate semver;
+extern crate toml;
+
+#[macro_use] extern crate libimagstore;
+extern crate libimagrt;
+extern crate libimagnotes;
+
+module_entry_path_mod!("diary", "0.1.0");
+
