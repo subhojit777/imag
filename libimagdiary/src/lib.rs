243aeb5e7226a36495871d6287d623e1f7dd6f41
Matthias Beyer <mail@beyermatthias.de>
2016-03-31 14:52:19 +0200

    Add dependencies

--- libimagdiary/src/lib.rs
+++ libimagdiary/src/lib.rs
@@ -1,10 +1,15 @@
+extern crate chrono;
 #[macro_use] extern crate log;
+#[macro_use] extern crate lazy_static;
 extern crate semver;
 extern crate toml;
+extern crate regex;
+extern crate itertools;
 
 #[macro_use] extern crate libimagstore;
+extern crate libimagutil;
+#[macro_use] extern crate libimagerror;
 extern crate libimagrt;
-extern crate libimagnotes;
 
 module_entry_path_mod!("diary", "0.1.0");
 
