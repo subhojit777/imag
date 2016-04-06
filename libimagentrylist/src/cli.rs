a41d037bcfb568a8166e572c747faf99ebf5d015
Matthias Beyer <mail@beyermatthias.de>
2016-04-06 11:48:01 +0200

    Add lister helper function

--- libimagentrylist/src/cli.rs
+++ libimagentrylist/src/cli.rs
@@ -1,4 +1,12 @@
-use clap::{Arg, App, SubCommand};
+use clap::{Arg, ArgMatches, App, SubCommand};
+
+use libimagstore::store::FileLockEntry;
+
+use result::Result;
+use listers::line::LineLister;
+use listers::path::PathLister;
+use lister::Lister;
+use error::{ListError, ListErrorKind};
 
 pub fn build_list_cli_component<'a, 'b>() -> App<'a, 'b> {
     SubCommand::with_name(list_subcommand_name())
@@ -48,3 +56,27 @@ pub fn list_backend_path_absolute() -> &'static str {
     "path-absolute"
 }
 
+// TODO: Add Registry for listers where a HashMap name->lister is in and where we can fetch the
+// lister from.
+pub fn list_entries_with_lister<'a, I>(m: &ArgMatches, entries: I) -> Result<()>
+    where I: Iterator<Item = FileLockEntry<'a>>
+{
+    if let Some(matches) = m.subcommand_matches(list_subcommand_name()) {
+        if matches.is_present(list_backend_line()) {
+            return LineLister::new("<unknown>").list(entries)
+        };
+
+        if matches.is_present(list_backend_path()) {
+            return PathLister::new(false).list(entries)
+        }
+
+
+        if matches.is_present(list_backend_path_absolute()) {
+            return PathLister::new(true).list(entries)
+        }
+
+        Ok(())
+    } else {
+        Err(ListError::new(ListErrorKind::CLIError, None))
+    }
+}
