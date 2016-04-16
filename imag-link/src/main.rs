10edc17e19231393f4cef2028ad5ec5b86a484c6
Matthias Beyer <mail@beyermatthias.de>
2016-04-16 22:46:46 +0200

    Enable lints

--- imag-link/src/main.rs
+++ imag-link/src/main.rs
@@ -1,3 +1,18 @@
+#![deny(
+    non_camel_case_types,
+    non_snake_case,
+    path_statements,
+    trivial_numeric_casts,
+    unstable_features,
+    unused_allocation,
+    unused_import_braces,
+    unused_imports,
+    unused_must_use,
+    unused_mut,
+    unused_qualifications,
+    while_true,
+)]
+
 #[macro_use] extern crate log;
 extern crate clap;
 #[macro_use] extern crate semver;
