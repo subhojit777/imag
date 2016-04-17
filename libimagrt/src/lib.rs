3bb4528060e68ac1736da67fac2f55e40e1f2b75
Matthias Beyer <mail@beyermatthias.de>
2016-04-17 20:48:22 +0200

    Enable lints

--- libimagrt/src/lib.rs
+++ libimagrt/src/lib.rs
@@ -1,4 +1,5 @@
 #![deny(
+    dead_code,
     non_camel_case_types,
     non_snake_case,
     path_statements,
@@ -7,6 +8,7 @@
     unused_allocation,
     unused_import_braces,
     unused_imports,
+    unused_must_use,
     unused_mut,
     unused_qualifications,
     while_true,
