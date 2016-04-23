14a4b786e8a525e28c3f12457029763c5bd77bea
Matthias Beyer <mail@beyermatthias.de>
2016-04-23 17:19:32 +0200

    Enable lints

--- libimagdiary/src/lib.rs
+++ libimagdiary/src/lib.rs
@@ -1,3 +1,19 @@
+#![deny(
+    dead_code,
+    non_camel_case_types,
+    non_snake_case,
+    path_statements,
+    trivial_numeric_casts,
+    unstable_features,
+    unused_allocation,
+    unused_import_braces,
+    unused_imports,
+    unused_must_use,
+    unused_mut,
+    unused_qualifications,
+    while_true,
+)]
+
 extern crate chrono;
 #[macro_use] extern crate log;
 #[macro_use] extern crate lazy_static;
