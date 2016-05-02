5ac661c3347e5aa3e141e3065e8c077053f4d34c
Andre Bogus <bogusandre@gmail.com>
2016-05-03 01:23:06 +0200

    fixed imag-link iterator error

--- imag-link/src/main.rs
+++ imag-link/src/main.rs
@@ -297,7 +297,7 @@ fn set_links_for_entry(store: &Store, matches: &ArgMatches, entry: &mut FileLock
 fn list_links_for_entry(store: &Store, entry: &mut FileLockEntry) {
     let res = entry.get_external_links(store)
         .and_then(|links| {
-            for (i, link) in links.enumerate() {
+            for (i, link) in links.iter().enumerate() {
                 println!("{: <3}: {}", i, link);
             }
             Ok(())
