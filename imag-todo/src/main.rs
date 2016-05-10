d68ed91be19393f49950cc05fbc85de122fb3efd
mario <mario-krehl@gmx.de>
2016-05-10 15:54:46 +0200

    add first implementation of command build

--- imag-todo/src/main.rs
+++ imag-todo/src/main.rs
@@ -1,3 +1,24 @@
+extern crate clap;
+extern crate glob;
+#[macro_use] extern crate log;
+extern crate semver;
+extern crate toml;
+#[macro_use] extern crate version;
+
+extern crate libimagrt;
+extern crate libimagstore;
+extern crate libimagutil;
+
+use std::process::exit;
+
+use libimagrt::runtime::Runtime;
+use libimagstore::store::FileLockEntry;
+use libimagutil::trace::trace_error;
+
+mod ui;
+
+use ui::build_ui;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ imag-todo/src/ui.rs
@@ -0,0 +1,42 @@
+use clap::{Arg, App, ArgGroup, SubCommand};
+
+pub fn build_ui<'a>(app: App<'a, 'a>) -> App<'a, 'a> {
+    app
+        .subcommand(SubCommand::with_name("tw-hook")
+                    .about("For use in a taskwarrior hook")
+                    .version("0.1")
+
+                    .arg(Arg::with_name("add")
+                         .long("add")
+                         .short("a")
+                         .takes_value(false)
+                         .required(false)
+                         .help("For use in an on-add hook"))
+
+                    .arg(Arg::with_name("delete")
+                         .long("delete")
+                         .short("d")
+                         .takes_value(false)
+                         .required(false)
+                         .help("For use in an on-delete hook"))
+
+                    .group(ArgGroup::with_name("taskwarrior hooks")
+                           .args(&[ "add",
+                                    "delete",
+                                ])
+                           .required(true))
+                    )
+
+        .subcommand(SubCommand::with_name("exec")
+                    .about("Send a command to taskwarrior")
+                    .version("0.1")
+
+                    .arg(Arg::with_name("command")
+                         .long("command")
+                         .short("c")
+                         .takes_value(true)
+                         .required(true)
+                         .help("Args written in the string will be send directly to taskwarrior"))
+                    )
+}
+
