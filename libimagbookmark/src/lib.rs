63d2f56a8893934c92a4ae64ad8ba3a1b5cd8f2e
Matthias Beyer <mail@beyermatthias.de>
2016-05-12 15:12:06 +0200

    Add dependencies

--- libimagbookmark/src/lib.rs
+++ libimagbookmark/src/lib.rs
@@ -1,6 +1,6 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
+#[macro_use] extern crate log;
+extern crate semver;
+extern crate url;
+
+#[macro_use] extern crate libimagstore;
+extern crate libimagentrylink;
