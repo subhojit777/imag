24e747029cec3773752490c5896cb5b9eec9dd19
Matthias Beyer <mail@beyermatthias.de>
2016-05-15 00:35:09 +0200

    Implement util::mkrepo()

--- libimagstorestdhook/src/vcs/git/util.rs
+++ libimagstorestdhook/src/vcs/git/util.rs
@@ -2,8 +2,20 @@
 //!
 //! Contains primitives to create a repository within the store path
 
+use git2::Repository;
+
+use vcs::git::error::GitHookErrorKind as GHEK;
+use vcs::git::error::MapErrInto;
+
 pub fn mkrepo(store: &Store) -> Result<()> {
-    unimplemented!()
+    let mut opts = RepositoryInitOptions::new();
+    opts.bare(false);
+    opts.no_reinit(true);
+    opts.mkdir(false);
+    opts.external_template(false);
+    Repository::init_opts(store.path(), &opts)
+        .map(|_| ())
+        .map_err_into(GHEK::MkRepo)
 }
 
 pub fn hasrepo(store: &Store) -> bool {
