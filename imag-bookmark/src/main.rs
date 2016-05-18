5f04981c91f700c4c3a3d2c2f13bc88a01af6991
Matthias Beyer <mail@beyermatthias.de>
2016-05-18 21:42:06 +0200

    Add dependencies

--- imag-bookmark/src/main.rs
+++ imag-bookmark/src/main.rs
@@ -1,3 +1,17 @@
+extern crate clap;
+#[macro_use] extern crate log;
+#[macro_use] extern crate version;
+
+extern crate libimagbookmark;
+extern crate libimagentrylink;
+extern crate libimagrt;
+extern crate libimagutil;
+
+mod ui;
+
+use ui:build_ui;
+
 fn main() {
     println!("Hello, world!");
 }
+
