d3dc4eb57d77a7d77131986466a6c13014042f41
Matthias Beyer <mail@beyermatthias.de>
2016-05-19 17:11:18 +0200

    libimagcounter: use generate_error_module!() macro and reexport generated types

--- libimagcounter/src/error.rs
+++ libimagcounter/src/error.rs
@@ -1,9 +1,12 @@
-generate_error_imports!();
-
-generate_error_types!(CounterError, CounterErrorKind,
-    StoreReadError          => "Store read error",
-    StoreWriteError         => "Store write error",
-    HeaderTypeError         => "Header type error",
-    HeaderFieldMissingError => "Header field missing error"
+generate_error_module!(
+    generate_error_types!(CounterError, CounterErrorKind,
+        StoreReadError          => "Store read error",
+        StoreWriteError         => "Store write error",
+        HeaderTypeError         => "Header type error",
+        HeaderFieldMissingError => "Header field missing error"
+    );
 );
+
+pub use self::error::CounterError;
+pub use self::error::CounterErrorKind;
 
