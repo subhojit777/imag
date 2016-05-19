22e015b8fd5805dcfbfde4308e79f4b82224be92
Matthias Beyer <mail@beyermatthias.de>
2016-05-19 17:05:14 +0200

    libimagcounter: Replace error module imports with macro helper

--- libimagcounter/src/error.rs
+++ libimagcounter/src/error.rs
@@ -1,6 +1,4 @@
-use std::error::Error;
-use std::fmt::Error as FmtError;
-use std::fmt::{Display, Formatter};
+generate_error_imports!();
 
 generate_error_types!(CounterError, CounterErrorKind,
     StoreReadError          => "Store read error",
