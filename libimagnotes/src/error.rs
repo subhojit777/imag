c88d5b308f59f44b6c561e6053edffd746912b0c
Matthias Beyer <mail@beyermatthias.de>
2016-05-19 17:05:14 +0200

    libimagnotes: Replace error module imports with macro helper

--- libimagnotes/src/error.rs
+++ libimagnotes/src/error.rs
@@ -1,6 +1,4 @@
-use std::error::Error;
-use std::fmt::Error as FmtError;
-use std::fmt::{Display, Formatter};
+generate_error_imports!();
 
 generate_error_types!(NoteError, NoteErrorKind,
     StoreWriteError       => "Error writing store",
