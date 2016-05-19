bfa1fc5c875570c8f251dcb6cf02e3c8fbe1cfbd
Matthias Beyer <mail@beyermatthias.de>
2016-05-19 17:11:18 +0200

    libimagnotes: use generate_error_module!() macro and reexport generated types

--- libimagnotes/src/error.rs
+++ libimagnotes/src/error.rs
@@ -1,9 +1,12 @@
-generate_error_imports!();
-
-generate_error_types!(NoteError, NoteErrorKind,
-    StoreWriteError       => "Error writing store",
-    StoreReadError        => "Error reading store",
-    HeaderTypeError       => "Header type error",
-    NoteToEntryConversion => "Error converting Note instance to Entry instance"
+generate_error_module!(
+    generate_error_types!(NoteError, NoteErrorKind,
+        StoreWriteError       => "Error writing store",
+        StoreReadError        => "Error reading store",
+        HeaderTypeError       => "Header type error",
+        NoteToEntryConversion => "Error converting Note instance to Entry instance"
+    );
 );
+
+pub use self::error::NoteError;
+pub use self::error::NoteErrorKind;
 
