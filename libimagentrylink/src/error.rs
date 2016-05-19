91f26220c65d61ad91a16e4d6befe839b971cc59
Matthias Beyer <mail@beyermatthias.de>
2016-05-19 17:05:14 +0200

    libimagentrylink: Replace error module imports with macro helper

--- libimagentrylink/src/error.rs
+++ libimagentrylink/src/error.rs
@@ -1,6 +1,4 @@
-use std::error::Error;
-use std::fmt::Error as FmtError;
-use std::fmt::{Display, Formatter};
+generate_error_imports!();
 
 generate_error_types!(LinkError, LinkErrorKind,
     EntryHeaderReadError    => "Error while reading an entry header",
