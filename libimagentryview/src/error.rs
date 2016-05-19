21dfa9fba2e4aef994c2b636e081acd99a2c1e52
Matthias Beyer <mail@beyermatthias.de>
2016-05-19 17:11:18 +0200

    libimagentryview: use generate_error_module!() macro and reexport generated types

--- libimagentryview/src/error.rs
+++ libimagentryview/src/error.rs
@@ -1,6 +1,9 @@
-generate_error_imports!();
-
-generate_error_types!(ViewError, ViewErrorKind,
-    Unknown => "Unknown view error"
+generate_error_module!(
+    generate_error_types!(ViewError, ViewErrorKind,
+        Unknown => "Unknown view error"
+    );
 );
+
+pub use self::error::ViewError;
+pub use self::error::ViewErrorKind;
 
