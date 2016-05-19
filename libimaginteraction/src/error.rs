81d46f9e74a47d8081a37424eb4dc862df9fe1f5
Matthias Beyer <mail@beyermatthias.de>
2016-05-19 17:05:14 +0200

    libimaginteraction: Replace error module imports with macro helper

--- libimaginteraction/src/error.rs
+++ libimaginteraction/src/error.rs
@@ -1,6 +1,4 @@
-use std::error::Error;
-use std::fmt::Error as FmtError;
-use std::fmt::{Display, Formatter};
+generate_error_imports!();
 
 generate_error_types!(InteractionError, InteractionErrorKind,
     Unknown => "Unknown Error"
