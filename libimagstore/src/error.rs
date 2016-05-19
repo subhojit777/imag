cc8642f54f4e9f49b00cc5ffcb3aab4b77aabc6e
Matthias Beyer <mail@beyermatthias.de>
2016-05-19 17:05:14 +0200

    libimagstore: Replace error module imports with macro helper

--- libimagstore/src/error.rs
+++ libimagstore/src/error.rs
@@ -1,6 +1,4 @@
-use std::error::Error;
-use std::fmt::Error as FmtError;
-use std::fmt::{Display, Formatter};
+generate_error_imports!();
 use std::convert::From;
 
 generate_error_types!(StoreError, StoreErrorKind,
