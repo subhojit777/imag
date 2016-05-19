6bc5099accf8a1404d74700ba128512f0d217516
Matthias Beyer <mail@beyermatthias.de>
2016-05-19 17:11:18 +0200

    imag-store: use generate_error_module!() macro and reexport generated types

--- imag-store/src/error.rs
+++ imag-store/src/error.rs
@@ -1,7 +1,10 @@
-generate_error_imports!();
-
-generate_error_types!(StoreError, StoreErrorKind,
-    BackendError      => "Backend Error",
-    NoCommandlineCall => "No commandline call"
+generate_error_module!(
+    generate_error_types!(StoreError, StoreErrorKind,
+        BackendError      => "Backend Error",
+        NoCommandlineCall => "No commandline call"
+    );
 );
+
+pub use self::error::StoreError;
+pub use self::error::StoreErrorKind;
 
