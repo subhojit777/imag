537fe88e13f0356bcf54f1f2109ca812097914a0
Matthias Beyer <mail@beyermatthias.de>
2016-05-19 17:11:18 +0200

    imag-view: use generate_error_module!() macro and reexport generated types

--- imag-view/src/error.rs
+++ imag-view/src/error.rs
@@ -1,9 +1,12 @@
-generate_error_imports!();
-
-generate_error_types!(ViewError, ViewErrorKind,
-    StoreError     => "Store error",
-    NoVersion      => "No version specified",
-    PatternError   => "Error in Pattern",
-    GlobBuildError => "Could not build glob() Argument"
+generate_error_module!(
+    generate_error_types!(ViewError, ViewErrorKind,
+        StoreError     => "Store error",
+        NoVersion      => "No version specified",
+        PatternError   => "Error in Pattern",
+        GlobBuildError => "Could not build glob() Argument"
+    );
 );
+
+pub use self::error::ViewError;
+pub use self::error::ViewErrorKind;
 
