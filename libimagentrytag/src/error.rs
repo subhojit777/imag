be3d39a65c02f941772046d3413b9384f8f7d464
Matthias Beyer <mail@beyermatthias.de>
2016-05-19 17:05:14 +0200

    libimagentrytag: Replace error module imports with macro helper

--- libimagentrytag/src/error.rs
+++ libimagentrytag/src/error.rs
@@ -1,6 +1,4 @@
-use std::error::Error;
-use std::fmt::Error as FmtError;
-use std::fmt::{Display, Formatter};
+generate_error_imports!();
 
 generate_error_types!(TagError, TagErrorKind,
     TagTypeError     => "Entry Header Tag Type wrong",
