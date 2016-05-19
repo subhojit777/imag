698c219e04677654be45849fe0a73429860d7e51
Matthias Beyer <mail@beyermatthias.de>
2016-05-19 17:11:18 +0200

    libimagentrylist: use generate_error_module!() macro and reexport generated types

--- libimagentrylist/src/error.rs
+++ libimagentrylist/src/error.rs
@@ -1,9 +1,12 @@
-generate_error_imports!();
-
-generate_error_types!(ListError, ListErrorKind,
-    FormatError    => "FormatError",
-    EntryError     => "EntryError",
-    IterationError => "IterationError",
-    CLIError       => "No CLI subcommand for listing entries"
+generate_error_module!(
+    generate_error_types!(ListError, ListErrorKind,
+        FormatError    => "FormatError",
+        EntryError     => "EntryError",
+        IterationError => "IterationError",
+        CLIError       => "No CLI subcommand for listing entries"
+    );
 );
+
+pub use self::error::ListError;
+pub use self::error::ListErrorKind;
 
