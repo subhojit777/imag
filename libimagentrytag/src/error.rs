b624ba9c06cb8fbbfe601576e1a99afa8f866b6b
Matthias Beyer <mail@beyermatthias.de>
2016-05-19 17:11:18 +0200

    libimagentrytag: use generate_error_module!() macro and reexport generated types

--- libimagentrytag/src/error.rs
+++ libimagentrytag/src/error.rs
@@ -1,9 +1,12 @@
-generate_error_imports!();
-
-generate_error_types!(TagError, TagErrorKind,
-    TagTypeError     => "Entry Header Tag Type wrong",
-    HeaderReadError  => "Error while reading entry header",
-    HeaderWriteError => "Error while writing entry header",
-    NotATag          => "String is not a tag"
+generate_error_module!(
+    generate_error_types!(TagError, TagErrorKind,
+        TagTypeError     => "Entry Header Tag Type wrong",
+        HeaderReadError  => "Error while reading entry header",
+        HeaderWriteError => "Error while writing entry header",
+        NotATag          => "String is not a tag"
+    );
 );
+
+pub use self::error::TagError;
+pub use self::error::TagErrorKind;
 
