cf393b4bde2d36bf30b438692f8535c5e6420151
Matthias Beyer <mail@beyermatthias.de>
2016-05-19 17:11:18 +0200

    libimagentrylink: use generate_error_module!() macro and reexport generated types

--- libimagentrylink/src/error.rs
+++ libimagentrylink/src/error.rs
@@ -1,13 +1,16 @@
-generate_error_imports!();
-
-generate_error_types!(LinkError, LinkErrorKind,
-    EntryHeaderReadError    => "Error while reading an entry header",
-    EntryHeaderWriteError   => "Error while writing an entry header",
-    ExistingLinkTypeWrong   => "Existing link entry has wrong type",
-    LinkTargetDoesNotExist  => "Link target does not exist in the store",
-    InternalConversionError => "Error while converting values internally",
-    InvalidUri              => "URI is not valid",
-    StoreReadError          => "Store read error",
-    StoreWriteError         => "Store write error"
+generate_error_module!(
+    generate_error_types!(LinkError, LinkErrorKind,
+        EntryHeaderReadError    => "Error while reading an entry header",
+        EntryHeaderWriteError   => "Error while writing an entry header",
+        ExistingLinkTypeWrong   => "Existing link entry has wrong type",
+        LinkTargetDoesNotExist  => "Link target does not exist in the store",
+        InternalConversionError => "Error while converting values internally",
+        InvalidUri              => "URI is not valid",
+        StoreReadError          => "Store read error",
+        StoreWriteError         => "Store write error"
+    );
 );
+
+pub use self::error::LinkError;
+pub use self::error::LinkErrorKind;
 
