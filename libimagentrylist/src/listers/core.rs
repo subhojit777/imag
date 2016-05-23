38031d5798d385eb2521e48f350509b97a3dd1b1
Matthias Beyer <mail@beyermatthias.de>
2016-05-23 19:16:05 +0200

    Add counter in libimagentrylist::listers::Core

--- libimagentrylist/src/listers/core.rs
+++ libimagentrylist/src/listers/core.rs
@@ -27,12 +27,19 @@ impl<'a> Lister for CoreLister<'a> {
         use error::ListError as LE;
         use error::ListErrorKind as LEK;
 
-        entries.fold(Ok(()), |accu, entry| {
-            accu.and_then(|_| {
-                    write!(stdout(), "{:?}\n", (self.lister)(&entry))
-                        .map_err(|e| LE::new(LEK::FormatError, Some(Box::new(e))))
-                })
-            })
+        debug!("Called list()");
+        let (r, n) = entries
+            .fold((Ok(()), 0), |(accu, i), entry| {
+                debug!("fold({:?}, {:?})", accu, entry);
+                let r = accu.and_then(|_| {
+                        debug!("Listing Entry: {:?}", entry);
+                        write!(stdout(), "{:?}\n", (self.lister)(&entry))
+                            .map_err(|e| LE::new(LEK::FormatError, Some(Box::new(e))))
+                    });
+                (r, i + 1)
+            });
+        debug!("Iterated over {} entries", n);
+        r
     }
 
 }
