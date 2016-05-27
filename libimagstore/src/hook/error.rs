cb87b4b5d8893c3ce856ddabb885e4eb30c6e777
Matthias Beyer <mail@beyermatthias.de>
2016-05-27 10:13:03 +0200

    Remove old error code which is unused

--- libimagstore/src/hook/error.rs
+++ libimagstore/src/hook/error.rs
@@ -6,16 +6,3 @@ generate_error_types!(HookError, HookErrorKind,
     AccessTypeViolation => "Hook access type violation"
 );
 
-pub trait IntoHookError {
-    fn into_hookerror(self) -> HookError;
-    fn into_hookerror_with_cause(self, cause: Box<Error>) -> HookError;
-}
-
-impl Into<HookError> for (HookErrorKind, Box<Error>) {
-
-    fn into(self) -> HookError {
-        HookError::new(self.0, Some(self.1))
-    }
-
-}
-
