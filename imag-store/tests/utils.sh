c098b065042c9bb1466a33ceca9a8221d4ee21cf
Matthias Beyer <mail@beyermatthias.de>
2016-06-07 20:46:49 +0200

    Fix: sourcing of test-util helper functions fixed

--- imag-store/tests/utils.sh
+++ imag-store/tests/utils.sh
@@ -1,4 +1,4 @@
-source $(dirname ${BASH_SOURCE[0]})/../tests/utils.sh
+source $(dirname ${BASH_SOURCE[0]})/../../tests/utils.sh
 
 imag-store() {
     imag-call-binary "$(dirname ${BASH_SOURCE[0]})/../target/debug/" imag-store $*
