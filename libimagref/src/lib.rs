6d41747fdb112d0aa730da5d5805547fe3f29330
Matthias Beyer <mail@beyermatthias.de>
2016-06-09 19:34:18 +0200

    Initial import

---
+++ libimagref/src/lib.rs
@@ -0,0 +1,15 @@
+#[macro_use] extern crate log;
+extern crate crypto;
+extern crate itertools;
+extern crate semver;
+extern crate toml;
+extern crate version;
+extern crate walkdir;
+
+#[macro_use] extern crate libimagstore;
+#[macro_use] extern crate libimagerror;
+#[macro_use] extern crate libimagutil;
+
+module_entry_path_mod!("ref", "0.1.0");
+
+pub mod ref;
