e63f17d51e4f025548a05069b32ae7fdead9ba16
Matthias Beyer <mail@beyermatthias.de>
2016-07-10 16:24:15 +0200

    Add dependency: libimagerror

--- libimagtodo/src/lib.rs
+++ libimagtodo/src/lib.rs
@@ -3,6 +3,7 @@ extern crate uuid;
 extern crate toml;
 
 #[macro_use] extern crate libimagstore;
+#[macro_use] extern crate libimagerror;
 extern crate task_hookrs;
 
 module_entry_path_mod!("todo", "0.1.0");
