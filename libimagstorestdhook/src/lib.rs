010109a3c3103ae888a2db388c1a998ea4bea1fd
Matthias Beyer <mail@beyermatthias.de>
2016-07-15 23:08:37 +0200

    Disable lints

--- libimagstorestdhook/src/lib.rs
+++ libimagstorestdhook/src/lib.rs
@@ -1,18 +1,18 @@
-#![deny(
-    dead_code,
-    non_camel_case_types,
-    non_snake_case,
-    path_statements,
-    trivial_numeric_casts,
-    unstable_features,
-    unused_allocation,
-    unused_import_braces,
-    unused_imports,
-    unused_must_use,
-    unused_mut,
-    unused_qualifications,
-    while_true,
-)]
+// #![deny(
+//     dead_code,
+//     non_camel_case_types,
+//     non_snake_case,
+//     path_statements,
+//     trivial_numeric_casts,
+//     unstable_features,
+//     unused_allocation,
+//     unused_import_braces,
+//     unused_imports,
+//     unused_must_use,
+//     unused_mut,
+//     unused_qualifications,
+//     while_true,
+// )]
 
 #[macro_use] extern crate log;
 extern crate toml;
