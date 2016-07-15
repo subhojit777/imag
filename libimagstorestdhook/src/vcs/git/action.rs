61c3519d452d9f6787cb6ad3d444b0eb8bb09769
Matthias Beyer <mail@beyermatthias.de>
2016-07-15 22:29:28 +0200

    Impl Debug, Clone, Display for StoreAction

--- libimagstorestdhook/src/vcs/git/action.rs
+++ libimagstorestdhook/src/vcs/git/action.rs
@@ -1,8 +1,24 @@
+use std::fmt::{Display, Formatter, Error};
+
+#[derive(Clone, Debug)]
 pub enum StoreAction {
     Create,
     Retrieve,
     Update,
     Delete,
-
-    // "Read" doesn't matter, as we do not use git on read actions, only when altering content.
 }
+
+impl Display for StoreAction {
+
+    fn fmt(&self, fmt: &mut Formatter) -> Result<(), Error> {
+        write!(fmt, "StoreAction: {}",
+                match *self {
+                    StoreAction::Create   => "create",
+                    StoreAction::Retrieve => "retrieve",
+                    StoreAction::Update   => "update",
+                    StoreAction::Delete   => "delete",
+                })
+    }
+
+}
+
