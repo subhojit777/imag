189ebb2aeaefc66399d9f6efea04d65234ab8a09
Matthias Beyer <mail@beyermatthias.de>
2016-07-16 18:36:29 +0200

    Add error kinds for repository interaction errors

--- libimagstorestdhook/src/vcs/git/error.rs
+++ libimagstorestdhook/src/vcs/git/error.rs
@@ -2,7 +2,20 @@ generate_error_module!(
     generate_error_types!(GitHookError, GitHookErrorKind,
         ConfigError => "Configuration Error",
         ConfigTypeError => "Configuration value type wrong",
-        MkRepo => "Repository creation error"
+        RepositoryError                   => "Error while interacting with git repository",
+        RepositoryBranchError             => "Error while interacting with git branch(es)",
+        RepositoryBranchNameFetchingError => "Error while fetching branch name",
+        RepositorySignatureFetchingError  => "Error while fetching Authors/Committers signature",
+        RepositoryIndexFetchingError      => "Error while fetching Repository Index",
+        RepositoryPathAddingError         => "Error while adding Path to Index",
+        RepositoryTreeWritingError        => "Error while writing repository tree",
+        RepositoryTreeFindingError        => "Error while finding repository tree",
+        RepositoryCommitFindingError      => "Error while finding commit",
+        RepositoryCommittingError         => "Error while committing",
+        HeadFetchError                    => "Error while getting HEAD",
+        NotOnBranch                       => "No Branch is checked out",
+        MkRepo => "Repository creation error",
+        MkSignature => "Error while building Signature object"
     );
 );
 
