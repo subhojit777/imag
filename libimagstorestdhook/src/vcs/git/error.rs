c09f0b302f851aa49a3b5d70ddf78ad5c55fa625
Matthias Beyer <mail@beyermatthias.de>
2016-07-16 17:56:34 +0200

    Add error kinds for configuration errors

--- libimagstorestdhook/src/vcs/git/error.rs
+++ libimagstorestdhook/src/vcs/git/error.rs
@@ -1,5 +1,7 @@
 generate_error_module!(
     generate_error_types!(GitHookError, GitHookErrorKind,
+        ConfigError => "Configuration Error",
+        ConfigTypeError => "Configuration value type wrong",
         MkRepo => "Repository creation error"
     );
 );
