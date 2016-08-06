0d2cd16e940ad4f7ea6b0836032a6c6c37c7908d
Matthias Beyer <mail@beyermatthias.de>
2016-08-06 10:11:42 +0200

    Print the imported string before the information output

--- imag-todo/src/main.rs
+++ imag-todo/src/main.rs
@@ -50,7 +50,7 @@ fn tw_hook(rt: &Runtime) {
         let stdin = stdin.lock(); // implements BufRead which is required for `Task::import()`
 
         match Task::import(rt.store(), stdin) {
-            Ok((_, uuid)) => info!("Task {} stored in imag", uuid),
+            Ok((_, line, uuid)) => info!("{}\nTask {} stored in imag", line, uuid),
             Err(e) => {
                 trace_error(&e);
                 exit(1);
