fa379a2fa730c495e30d564a95d6a3223b00c5fc
Matthias Beyer <mail@beyermatthias.de>
2016-08-07 15:11:48 +0200

    Only return subcommand name

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -84,7 +84,7 @@ fn get_commands() -> Vec<String> {
                         .filter_map(|path| {
                            path.file_name()
                                .to_str()
-                               .and_then(|s| s.splitn(2, "-").nth(1).map(|s| format!("imag {}", s)))
+                               .and_then(|s| s.splitn(2, "-").nth(1).map(String::from))
                         })
                         .collect()
                 })
