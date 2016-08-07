1af3ab018b229053923aa7ab1cbf7bba638539d6
Matthias Beyer <mail@beyermatthias.de>
2016-08-07 16:54:48 +0200

    Use StoreId::new() in macro

--- libimagstore/src/storeid.rs
+++ libimagstore/src/storeid.rs
@@ -151,7 +151,7 @@ macro_rules! module_entry_path_mod {
 
             impl $crate::storeid::IntoStoreId for ModuleEntryPath {
                 fn into_storeid(self) -> $crate::storeid::StoreId {
-                    StoreId::from(self.0)
+                    StoreId::new(None, self.0)
                 }
             }
         }
