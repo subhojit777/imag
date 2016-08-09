b53f12c0a6f9e98aa77684f4e4ad01cbf68476f1
Matthias Beyer <mail@beyermatthias.de>
2016-08-09 15:38:27 +0200

    Add dependencies

--- libimagmail/src/lib.rs
+++ libimagmail/src/lib.rs
@@ -1,6 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
+#[macro_use] extern crate log;
+extern crate mailparse;
+extern crate semver;
+extern crate toml;
+
+#[macro_use] extern crate libimagerror;
+extern crate libimagstore;
+
+pub mod mail;
+pub mod error;
+pub mod result;
+
