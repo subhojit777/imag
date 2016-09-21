c7f890d0d4b014c82925514a1679ccc5a9c42d98
Matthias Beyer <mail@beyermatthias.de>
2016-09-21 19:25:25 +0200

    Add error kind for ref handling errors

--- libimagmail/src/error.rs
+++ libimagmail/src/error.rs
@@ -1,6 +1,7 @@
 generate_error_module!(
     generate_error_types!(MailError, MailErrorKind,
         RefCreationError => "Error creating a reference to a file/directory",
+        RefHandlingError => "Error while handling the internal reference object",
         MailParsingError => "Error while parsing mail",
 
         FetchByHashError => "Error fetching mail from Store by hash",
