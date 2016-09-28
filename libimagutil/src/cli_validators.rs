d6f705dc9036bcb9777530aa259c83ff0e11293d
Matthias Beyer <mail@beyermatthias.de>
2016-09-28 19:51:21 +0200

    Add is_tag clap validator helper

--- libimagutil/src/cli_validators.rs
+++ libimagutil/src/cli_validators.rs
@@ -24,3 +24,12 @@ pub fn is_url(s: String) -> Result<(), String> {
     Url::parse(&s).map(|_| ()).map_err(|_| format!("Not a URL: {}", s))
 }
 
+pub fn is_tag(s: String) -> Result<(), String> {
+    use regex::Regex;
+    lazy_static! { static ref TAG_RE : Regex = Regex::new("[:alpha:][:word:]*").unwrap(); }
+
+    TAG_RE
+        .is_match(&s)
+        .as_result((), format!("Not a valid Tag: '{}' - Valid is [a-zA-Z][0-9a-zA-Z]*", s))
+}
+
