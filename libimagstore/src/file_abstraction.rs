27e816fc69f978a26f502aca9f4d824d1e682977
Matthias Beyer <mail@beyermatthias.de>
2016-10-11 21:17:48 +0200

    Do not simply unwrap, but return error in case of error

--- libimagstore/src/file_abstraction.rs
+++ libimagstore/src/file_abstraction.rs
@@ -69,14 +69,14 @@ mod fs {
 
         pub fn copy(from: &PathBuf, to: &PathBuf) -> Result<(), SE> {
             let mut map = MAP.lock().unwrap();
-            let a = map.get(from).unwrap().clone();
+            let a = try!(map.get(from).cloned().ok_or(SEK::FileNotFound.into_error()));
             map.insert(to.clone(), a);
             Ok(())
         }
 
         pub fn rename(from: &PathBuf, to: &PathBuf) -> Result<(), SE> {
             let mut map = MAP.lock().unwrap();
-            let a = map.get(from).unwrap().clone();
+            let a = try!(map.get(from).cloned().ok_or(SEK::FileNotFound.into_error()));
             map.insert(to.clone(), a);
             Ok(())
         }
