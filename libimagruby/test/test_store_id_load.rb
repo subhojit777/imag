4fde23bc1b19c571ac8d74742559f480f9fb034e
Matthias Beyer <mail@beyermatthias.de>
2017-01-21 12:21:32 +0100

    Fix test script for new location of build articfact

--- libimagruby/test/test_store_id_load.rb
+++ libimagruby/test/test_store_id_load.rb
@@ -2,7 +2,7 @@
 
 require 'fiddle'
 
-lib = Fiddle::dlopen './target/debug/libimag_ruby.so'
+lib = Fiddle::dlopen '../target/release/liblibimagruby.so'
 Fiddle::Function::new(lib['imag_ruby_initialize'], [], Fiddle::TYPE_VOIDP).call
 
 works = (not RStoreId.new_baseless("test").nil?)
