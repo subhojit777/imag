0215817262687fa2d1b3901502fdcd50b2264eef
Matthias Beyer <mail@beyermatthias.de>
2017-07-15 00:05:05 +0200

    Add quickcheck-based test for link removal

--- libimagentrylink/src/lib.rs
+++ libimagentrylink/src/lib.rs
@@ -42,6 +42,10 @@ extern crate crypto;
 #[cfg(test)]
 extern crate env_logger;
 
+#[cfg(test)]
+#[macro_use]
+extern crate quickcheck;
+
 #[macro_use] extern crate libimagstore;
 #[macro_use] extern crate libimagerror;
 extern crate libimagutil;
