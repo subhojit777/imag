2c4dbc0a4ad002f1847e6a58b2dabefadd318df8
Matthias Beyer <mail@beyermatthias.de>
2017-07-15 20:54:28 +0200

    Implement "track" subcommand

--- imag-timetrack/src/track.rs
+++ imag-timetrack/src/track.rs
@@ -17,9 +17,59 @@
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 //
 
+use std::str::FromStr;
+
+use clap::ArgMatches;
+use chrono::naive::NaiveDateTime;
+
 use libimagrt::runtime::Runtime;
+use libimagerror::trace::trace_error;
+use libimagentrytimetrack::tag::TimeTrackingTag;
+use libimagentrytimetrack::timetrackingstore::TimeTrackStore;
+use libimagerror::trace::MapErrTrace;
 
 pub fn track(rt: &Runtime) -> i32 {
-    unimplemented!()
+    let (_, cmd) = rt.cli().subcommand();
+    let cmd = cmd.unwrap(); // checked in main()
+
+    // Gets the appropriate time from the commandline or None on error (errors already logged, so
+    // callee can directly return in case of error
+    fn get_time(cmd: &ArgMatches, clap_name: &str, errname: &str) -> Option<NaiveDateTime> {
+        let val = cmd
+            .value_of(clap_name)
+            .map(::chrono::naive::NaiveDateTime::from_str)
+            .unwrap(); // clap has our back
+
+        match val {
+            Ok(ndt) => Some(ndt),
+            Err(e)  => {
+                trace_error(&e);
+                error!("Cannot continue, not having {} time", errname);
+                None
+            },
+        }
+    }
+
+    let start = match get_time(&cmd, "start-time", "start") {
+        Some(t) => t,
+        None    => return 1,
+    };
+
+    let stop = match get_time(&cmd, "stop-time", "stop") {
+        Some(t) => t,
+        None    => return 1,
+    };
+
+    cmd.values_of("tags")
+        .unwrap() // enforced by clap
+        .map(String::from)
+        .map(TimeTrackingTag::from)
+        .fold(0, |acc, ttt| {
+            rt.store()
+              .create_timetracking(&start, &stop, &ttt)
+              .map_err_trace()
+              .map(|_| acc)
+              .unwrap_or(1)
+        })
 }
 
