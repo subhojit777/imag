f324deb41ec92040a703605c167ff4761b6f6a15
Matthias Beyer <mail@beyermatthias.de>
2018-01-02 13:02:09 +0100

    imag-init: initial import

---
+++ bin/core/imag-init/src/ui.rs
@@ -0,0 +1,49 @@
+//
+// imag - the personal information management suite for the commandline
+// Copyright (C) 2015, 2016 Matthias Beyer <mail@beyermatthias.de> and contributors
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; version
+// 2.1 of the License.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+use clap::{Arg, App};
+
+pub fn build_ui<'a>() -> App<'a, 'a> {
+    App::new("imag-init")
+        .version(&version!()[..])
+        .author("Matthias Beyer <mail@beyermatthias.de>")
+        .about("Initialize a ~/.imag repository. Optionally with git")
+
+        .arg(Arg::with_name("devel")
+             .long("dev")
+             .takes_value(false)
+             .required(false)
+             .multiple(false)
+             .help("Put dev configuration into the generated repo (with debugging enabled)"))
+
+        .arg(Arg::with_name("nogit")
+             .long("no-git")
+             .takes_value(false)
+             .required(false)
+             .multiple(false)
+             .help("Do not initialize git repository, even if 'git' executable is in $PATH"))
+
+        .arg(Arg::with_name("path")
+             .long("path")
+             .takes_value(true)
+             .required(false)
+             .multiple(false)
+             .help("Alternative path where to put the repository. Default: ~/.imag"))
+}
+
