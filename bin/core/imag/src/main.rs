85d1c632d3d0c5c122bb4a6c11b5011273656ffa
Matthias Beyer <mail@beyermatthias.de>
2018-02-06 19:28:08 +0100

    Print "imag-<tool>" rather than only "<tool>"
    The error output should note "imag-<tool> exited with ..." rather than
    only "<tool> exited with ..."

--- bin/core/imag/src/main.rs
+++ bin/core/imag/src/main.rs
@@ -219,8 +219,8 @@ fn main() {
             {
                 Ok(exit_status) => {
                     if !exit_status.success() {
-                        debug!("{} exited with non-zero exit code: {:?}", subcommand, exit_status);
-                        println!("{} exited with non-zero exit code", subcommand);
+                        debug!("imag-{} exited with non-zero exit code: {:?}", subcommand, exit_status);
+                        println!("imag-{} exited with non-zero exit code", subcommand);
                         exit(exit_status.code().unwrap_or(1));
                     }
                     debug!("Successful exit!");
