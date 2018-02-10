30d2c28a22f3b8b6581e9e565330420f7cb95d94
Matthias Beyer <mail@beyermatthias.de>
2018-02-10 16:51:31 +0100

    Update version string: 0.6.0 -> 0.7.0

--- bin/core/imag-link/src/main.rs
+++ bin/core/imag-link/src/main.rs
@@ -265,7 +265,7 @@ mod tests {
     make_mock_app! {
         app "imag-link";
         modulename mock;
-        version "0.6.0";
+        version "0.7.0";
         with help "imag-link mocking app";
     }
     use self::mock::generate_test_runtime;
