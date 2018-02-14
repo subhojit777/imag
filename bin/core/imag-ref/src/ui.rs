d3d6b80b8be7d9b3de95043d2c3dffd23b5c77f3
Matthias Beyer <mail@beyermatthias.de>
2018-02-14 12:46:13 +0100

    Redesign of the CLI

--- bin/core/imag-ref/src/ui.rs
+++ bin/core/imag-ref/src/ui.rs
@@ -19,73 +19,33 @@
 
 use clap::{Arg, App, SubCommand};
 
-use libimagutil::cli_validators::is_existing_path;
-
 pub fn build_ui<'a>(app: App<'a, 'a>) -> App<'a, 'a> {
     app
-        .subcommand(SubCommand::with_name("add")
-                    .about("Add a reference to a file outside of the store")
+        .subcommand(SubCommand::with_name("deref")
+                    .about("'Dereference' a ref. This prints the Path of the referenced file")
                     .version("0.1")
-                    .arg(Arg::with_name("path")
+                    .arg(Arg::with_name("ID")
                          .index(1)
                          .takes_value(true)
                          .required(true)
-                         .help("The path of the file")
-                         .validator(is_existing_path)
-                         .value_name("PATH"))
-                    .arg(Arg::with_name("track-content")
-                         .long("content-hash")
-                         .short("C")
-                         .takes_value(false)
-                         .required(false)
-                         .help("Hash the content for the reference"))
-                    .arg(Arg::with_name("track-permissions")
-                         .long("permission-tracking")
-                         .short("P")
-                         .takes_value(false)
-                         .required(false)
-                         .help("Rememeber the permissions of the referenced file"))
+                         .help("The id of the store entry to dereference")
+                         .value_name("ID"))
                     )
 
         .subcommand(SubCommand::with_name("remove")
-                .about("Remove a reference")
+                .about("Remove a reference from an entry")
                 .version("0.1")
-                .arg(Arg::with_name("hash")
+                .arg(Arg::with_name("ID")
                      .index(1)
                      .takes_value(true)
                      .required(true)
-                     .help("Remove the reference with this hash")
-                     .value_name("HASH"))
+                     .multiple(true)
+                     .help("Remove the reference from this store entry")
+                     .value_name("ENTRIES"))
 
                 .arg(Arg::with_name("yes")
                      .long("yes")
                      .short("y")
                      .help("Don't ask whether this really should be done"))
                 )
-
-        .subcommand(SubCommand::with_name("list")
-                    .about("List references in the store")
-                    .version("0.1")
-
-                    .arg(Arg::with_name("check-dead")
-                         .long("check-dead")
-                         .short("d")
-                         .help("Check each reference whether it is dead"))
-
-                    .arg(Arg::with_name("check-changed")
-                         .long("check-changed")
-                         .short("c")
-                         .help("Check whether a reference had changed (content or permissions)"))
-
-                    .arg(Arg::with_name("check-changed-content")
-                         .long("check-changed-content")
-                         .short("C")
-                         .help("Check whether the content of the referenced file changed"))
-
-                    .arg(Arg::with_name("check-changed-permissions")
-                         .long("check-changed-perms")
-                         .short("P")
-                         .help("Check whether the permissions of the referenced file changed"))
-
-                    )
 }
