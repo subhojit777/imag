eeb2ef7db3969499bfcd2054586f709950f7238a
Matthias Beyer <mail@beyermatthias.de>
2018-02-26 00:09:34 +0100

    Implement CalendarCRUD for CalendarStore

--- lib/domain/libimagcalendar/src/store/calendars.rs
+++ lib/domain/libimagcalendar/src/store/calendars.rs
@@ -21,25 +21,38 @@ use std::path::Path;
 
 use libimagstore::store::FileLockEntry;
 use libimagstore::store::Store;
+use libimagentryref::refstore::RefStore;
+use libimagentryref::generators::sha1::Sha1;
 
 use error::Result;
+use error::CalendarError as CE;
 use store::CalendarCRUD;
 
 /// A interface to the store which offers CRUD functionality for calendars
 pub struct CalendarStore<'a>(&'a Store);
 
+make_unique_ref_path_generator! (
+    pub CalendarHasher
+    over Sha1
+    => with error CE
+    => with collection name "calendar"
+    => |path| {
+        Sha1::hash_n_bytes(path, 4096).map_err(CE::from)
+    }
+);
+
 impl<'a> CalendarCRUD<'a> for CalendarStore<'a> {
 
     fn get<H: AsRef<str>>(&self, hash: H) -> Result<Option<FileLockEntry<'a>>> {
-        unimplemented!()
+        self.0.get_ref::<CalendarHasher, H>(hash).map_err(CE::from)
     }
 
     fn create<P: AsRef<Path>>(&self, p: P) -> Result<FileLockEntry<'a>> {
-        unimplemented!()
+        self.0.create_ref::<CalendarHasher, P>(p).map_err(CE::from)
     }
 
     fn retrieve<P: AsRef<Path>>(&self, p: P) -> Result<FileLockEntry<'a>> {
-        unimplemented!()
+        self.0.retrieve_ref::<CalendarHasher, P>(p).map_err(CE::from)
     }
 
     fn delete_by_hash(&self, hash: String) -> Result<()> {
