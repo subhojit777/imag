c4ff8d5068a4860eca73a19d08f70d5b0d558bc0
Matthias Beyer <mail@beyermatthias.de>
2018-03-19 21:08:10 +0100

    Implement "collection add"

--- bin/domain/imag-calendar/src/collection.rs
+++ bin/domain/imag-calendar/src/collection.rs
@@ -18,31 +18,76 @@
 //
 
 use std::path::PathBuf;
+use std::process::exit;
+
+use walkdir::WalkDir;
+use walkdir::DirEntry;
+use clap::ArgMatches;
+
+use libimagrt::runtime::Runtime;
+use libimagerror::trace::MapErrTrace;
+use libimagcalendar::store::calendars::CalendarStore;
+use libimagcalendar::store::collections::CalendarCollectionStore;
 
 pub fn collection(rt: &Runtime) {
     let scmd = rt.cli().subcommand_matches("collection").unwrap(); // safed by main()
 
-    scmd.subcommand_name()
-        .map(|name| match name {
-            "add" => add(scmd),
-            "remove" => remove(scmd),
-            "show" => show(scmd),
-            "list" => list(scmd),
-            "find" => find(scmd),
-            _ => {
-                unimplemented!()
-            }
-        })
-        .unwrap_or_else(|| unreachable!("BUG, please report"))
+    match scmd.subcommand() {
+        ("add", scmd)    => add(rt, scmd.unwrap()),
+        ("remove", scmd) => remove(rt, scmd.unwrap()),
+        ("show", scmd)   => show(rt, scmd.unwrap()),
+        ("list", scmd)   => list(rt, scmd.unwrap()),
+        ("find", scmd)   => find(rt, scmd.unwrap()),
+        _ => {
+            unimplemented!()
+        }
+    }
 }
 
 fn add<'a>(rt: &Runtime, scmd: &ArgMatches<'a>) {
     let name = scmd.value_of("collection-add-name").map(String::from).unwrap(); // safe by clap
     let path = scmd.value_of("collection-add-path").map(PathBuf::from).unwrap(); // safe by clap
 
-    rt.store()
-        .retrieve_calendar_collection(path)
-        .map_err_trace_unwrap_exit(1)
+    if !path.is_dir() { // TODO: Move this check to libimagcalendar
+        error!("Path '{:?}' is not a directory", path);
+        exit(1)
+    } else {
+
+        let mut collection = rt.store()
+            .retrieve_calendar_collection(path.clone())
+            .map_err_trace_exit_unwrap(1);
+
+        info!("Collection added");
+
+        let is_not_hidden = |entry: &DirEntry| !entry
+            .file_name()
+            .to_str()
+            .map(|s| s.starts_with("."))
+            .unwrap_or(false);
+
+        for entry in WalkDir::new(path).follow_links(false).into_iter().filter_entry(is_not_hidden) {
+            match entry {
+                Ok(de) => {
+                    if de.file_type().is_file() {
+                        let entry = collection
+                            .add_retrieve_calendar_from_path(de.path(), rt.store())
+                            .map_err_trace_exit_unwrap(1);
+
+                        info!("Created entry: {} -> {}", entry.get_location(), de.path().display());
+                    } else {
+                        debug!("Ignored: {}", de.path().display());
+                        /* ignored */
+                    }
+                },
+
+                Err(e) => {
+                    error!("Error processing directory entry: {:?}", e);
+                },
+            }
+        }
+
+        debug!("Ready");
+    }
 }
 
 fn remove<'a>(rt: &Runtime, scmd: &ArgMatches<'a>) {
