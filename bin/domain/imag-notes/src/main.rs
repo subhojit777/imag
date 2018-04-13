f92163f6d037fea348fe5907eb369d40c3808073
Matthias Beyer <mail@beyermatthias.de>
2018-04-14 00:39:43 +0200

    Add lints

--- bin/domain/imag-notes/src/main.rs
+++ bin/domain/imag-notes/src/main.rs
@@ -17,6 +17,21 @@
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 //
 
+#![deny(
+    non_camel_case_types,
+    non_snake_case,
+    path_statements,
+    trivial_numeric_casts,
+    unstable_features,
+    unused_allocation,
+    unused_import_braces,
+    unused_imports,
+    unused_must_use,
+    unused_mut,
+    unused_qualifications,
+    while_true,
+)]
+
 extern crate clap;
 #[macro_use] extern crate log;
 extern crate itertools;
