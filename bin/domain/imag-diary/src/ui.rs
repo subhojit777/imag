58a6eea573c6887921d7883ed009662439f87c66
Matthias Beyer <mail@beyermatthias.de>
2018-04-30 14:04:15 +0200

    Remove the edit command
    The edit command of imag-diary gets removed by this patch.
    
    The rationale is that imag-edit is way more powerful and thus, not every
    other imag module should implement an edit command. Chaining
    `imag-diary list` and `imag edit -I` is encouraged.
    
    We still need the edit functionality in imag-diary, as `imag diary
    create` shall spawn an editor, still. We only remove the ability for
    editing of existing entries with this patch.

--- bin/domain/imag-diary/src/ui.rs
+++ bin/domain/imag-diary/src/ui.rs
@@ -77,19 +77,6 @@ pub fn build_ui<'a>(app: App<'a, 'a>) -> App<'a, 'a> {
                             .requires("timed"))
                    )
 
-        .subcommand(SubCommand::with_name("edit")
-                   .about("Edit a diary entry")
-                   .version("0.1")
-                   .arg(Arg::with_name("datetime")
-                        .long("datetime")
-                        .short("d")
-                        .takes_value(true)
-                        .required(false)
-                        .help("Specify the date and time which entry should be edited. If none is
-                        specified, the last entry is edited. If the diary entry does not exist for
-                        this time, this fails. Format: YYYY-MM-DDT[HH[:mm[:ss]]]"))
-                   )
-
         .subcommand(SubCommand::with_name("list")
                    .about("List diary entries")
                    .version("0.1"))
